54f4635b7550192ccc02c36828269a1b5819144d
Andrey <kezin636@gmail.com>
2022-08-01 22:24:02 +0300

    create pixel  base

--- Pixel.cs
+++ Pixel.cs
@@ -1,7 +1,35 @@
-﻿namespace ConsoleLib
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleLib
 {
     public class Pixel
     {
+        public override string ToString() => GetSymbol();
 
+        public string GetSymbol()
+        {
+            try
+            {
+                return brightnessCharacters[this.Brightness];
+            }
+            catch (KeyNotFoundException)
+            {
+                if (this.Brightness > 4)
+                    return brightnessCharacters[4];
+
+                return brightnessCharacters[0];
+            }
+        }
+
+        public int Brightness;
+        private Dictionary<int, string> brightnessCharacters = new Dictionary<int, string>()
+        { 
+            [0] = " ",
+            [1] = ".",
+            [2] = "r",
+            [3] = "g",
+            [4] = "@"
+        };
     }
 }
