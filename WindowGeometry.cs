07b4d908f54aab1c1af695f9cb304de14fdf64c8
Andrey <kezin636@gmail.com>
2022-08-01 21:46:44 +0300

    Добавьте файлы проекта.

---
+++ WindowGeometry.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleLib
+{
+    public class WindowGeometry
+    {
+        public WindowGeometry(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+
+        public int Width;
+        public int Height;
+    }
+}
